1abf31823d56f0a2f032987ca1b1311b7fb758e8
RileyG001 <102997683+RileyG001@users.noreply.github.com>
2022-07-07 21:56:30 -0700

    Create local_sub.sh

---
+++ local_sub.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+simpleAnalysis -a VBS /uclhc-2/data/uclhc/uci/user/ucigroup/VBS_H_MCsamples/submitDir_2022-06-01_MCTruth3_VBS_WpmWpmHjj_e8307_p4189_v01/output_0/DAOD_TRUTH3.VBS_WpmWpmHjj_0.pool.root,/uclhc-2/data/uclhc/uci/user/ucigroup/VBS_H_MCsamples/submitDir_2022-06-01_MCTruth3_VBS_WpmWpmHjj_e8307_p4189_v01/output_1/DAOD_TRUTH3.VBS_WpmWpmHjj_1.pool.root
+echo Done
